a4237bd27c68306b2a64d7a135000fd7f9c8a8e1
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-09-24 10:25:41 +0530

    Add faker and conditional success message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.enteredText = value;
+  }
 }
